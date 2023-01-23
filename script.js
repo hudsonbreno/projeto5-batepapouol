a3820f186dda2e1dff2555e3910a1665cb756f72
Breno <brennoohudson@gmail.com>
2023-01-23 15:45:10 -0300

    Enviar mensangem

--- script.js
+++ script.js
@@ -8,9 +8,42 @@ function erro (erro){
     window.location.reload();
 }
 
+function conversaNoServidor(resposta){
+    let batePapo = document.querySelector('.conversa');
+    batePapo.innerHTML = ''; 
+    for (let i = 0; i < 100; i++){
+    let hora = resposta.data[i].time;
+        let nome1 = resposta.data[i].from;
+        let nome2 = resposta.data[i].to;
+        let texto = resposta.data[i].text;
+        let tipo = resposta.data[i].type;
+
+        if (tipo === 'status' || tipo === 'message' ){
+        batePapo.innerHTML += `
+            
+        <div data-test="message" class="${tipo}">
+            (${hora}) ${nome1} para ${nome2}: ${texto}
+        </div>
+        
+        `;
+    }
+
+        if (tipo === 'private_message' && (nome1 === nome || nome2 === nome)){
+            batePapo.innerHTML += `
+
+            <div data-test="message" class="${tipo}">
+                (${hora}) ${nome1} para ${nome2}: ${texto}
+            </div>
+        '
+            `;
+        }
+    }
+
+        batePapo.querySelector('div:last-child').scrollIntoView();
+    }
 function pegarConversaNoServidor(){
      const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-     promessa.then(); //se deu certo
+     promessa.then(conversaNoServidor); 
      promessa.catch(erro);
 }
 
@@ -28,7 +61,26 @@ function mantendoConexao(){
 
 
 function usuarioVerificado (){
-    console.log("pegar conversar no servidor");
+    pegarConversaNoServidor();
+    setInterval(function (){ 
+        pegarConversaNoServidor();
+    } ,3000)
+    setInterval(mantendoConexao, 5000);
 }
+
+function enviarMensagem(){
+    const msg = {
+        from: nome,
+        to: "Todos",
+        text: mensagemDigitada.value,
+        type: "message" 
+    }
+    const enviar = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', msg);
+    mensagemDigitada.value = "";
+    enviar.then(pegarConversaNoServidor);
+    enviar.catch(erro);
+}
+
+
 
 verificarUsuario();
